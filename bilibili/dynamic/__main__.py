91a23ff28c111da4f0f11179e7052eaeddd4abe3
Zhengyang Song <songzy_thu@163.com>
2023-07-16 22:09:19 +0800

    Use os.path to construct output path, also refactor the output path.

--- bilibili/dynamic/__main__.py
+++ bilibili/dynamic/__main__.py
@@ -6,53 +6,58 @@ import json
 from datetime import datetime
 
 from .config import MID, COOKIE
-from .dynamic_util import build_dynamic_api_url, build_next_dynamic_api_url, extract_picture_urls
+from .dynamic_api_util import build_dynamic_api_url, build_next_dynamic_api_url, extract_picture_urls
 
-OUTPUT_DIR = 'bilibili/output'
+OUTPUT_ROOT_DIR = 'output/dynamic'
 URLS_FILENAME = 'picture_urls.json'
 
-HEADERS = {
-    "User-Agent":
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0",
-    "Accept":
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Upgrade-Insecure-Requests": "1",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
-    "Cookie": COOKIE
-}
 
+def extract_all_picture_urls(mid, cookie):
+    headers = {
+        "User-Agent":
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0",
+        "Accept":
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Cookie": cookie
+    }
 
-def extract_all_picture_urls():
     picture_urls = {}
 
-    dynamic_api_url = build_dynamic_api_url(MID)
-    print(dynamic_api_url)
+    dynamic_api_url = build_dynamic_api_url(mid)
+    print(f"fetching {dynamic_api_url}")
     while dynamic_api_url != '':
-        resp = requests.get(dynamic_api_url, headers=HEADERS).json()
+        resp = requests.get(dynamic_api_url, headers=headers).json()
 
         current_picture_urls = extract_picture_urls(resp)
         picture_urls = picture_urls | current_picture_urls
 
-        dynamic_api_url = build_next_dynamic_api_url(MID, resp)
-        print(dynamic_api_url)
+        dynamic_api_url = build_next_dynamic_api_url(mid, resp)
+        print(f"fetching {dynamic_api_url}")
 
         time.sleep(3)
 
     return picture_urls
 
 
-def download_picture(url, filepath):
+def make_parent_dirs_if_not_exist(filepath):
     parent_dir = os.path.dirname(filepath)
     if not os.path.exists(parent_dir):
         os.makedirs(parent_dir, exist_ok=True)
+
+
+def download_picture(url, filepath):
     print(url, filepath)
 
     time.sleep(1)
     response = requests.get(url)
+
+    make_parent_dirs_if_not_exist(filepath)
     with open(filepath, "wb") as f:
         f.write(response.content)
 
@@ -64,21 +69,23 @@ def download_pictures(picture_urls):
             picture_name = os.path.basename(url)
             download_picture(
                 url,
-                f'{OUTPUT_DIR}/{MID}/{pub_dt.strftime("%Y%m%d_%H%M%S")}/{index}_{picture_name}'
-            )
+                os.path.join(OUTPUT_ROOT_DIR, str(MID),
+                             pub_dt.strftime("%Y%m%d_%H%M%S"),
+                             f'{index}_{picture_name}'))
 
 
 if __name__ == '__main__':
-    URLS_PATH = f"{OUTPUT_DIR}/{URLS_FILENAME}"
-
     picture_urls = {}
-    if not os.path.exists(URLS_PATH):
-        picture_urls = extract_all_picture_urls()
 
-        with open(URLS_PATH, mode='w', encoding='utf-8') as f:
+    urls_filepath = os.path.join(OUTPUT_ROOT_DIR, str(MID), URLS_FILENAME)
+    if not os.path.exists(urls_filepath):
+        picture_urls = extract_all_picture_urls(MID, COOKIE)
+
+        make_parent_dirs_if_not_exist(urls_filepath)
+        with open(urls_filepath, mode='w', encoding='utf-8') as f:
             json.dump(picture_urls, f, ensure_ascii=False, indent=4)
 
-    with open(URLS_PATH) as f:
+    with open(urls_filepath) as f:
         picture_urls = json.loads(f.read())
 
-    download_pictures(picture_urls)
+    # download_pictures(picture_urls)
